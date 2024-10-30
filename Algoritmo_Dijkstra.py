e79205135424b0d555bbd4aa9a2de875e4e355c8
Abdiel <maganaabdiel02@gmail.com>
2024-10-29 23:52:57 -0600

    Create Algoritmo_Dijkstra.py

---
+++ Algoritmo_Dijkstra.py
@@ -0,0 +1,30 @@
+import heapq
+
+def dijkstra(grafo, inicio):
+    distancias = {nodo: float('infinity') for nodo in grafo}
+    distancias[inicio] = 0
+    cola_prioridad = [(0, inicio)]
+    
+    while cola_prioridad:
+        distancia_actual, nodo_actual = heapq.heappop(cola_prioridad)
+        if distancia_actual > distancias[nodo_actual]:
+            continue
+        
+        for vecino, peso in grafo[nodo_actual].items():
+            distancia = distancia_actual + peso
+            if distancia < distancias[vecino]:
+                distancias[vecino] = distancia
+                heapq.heappush(cola_prioridad, (distancia, vecino))
+    
+    return distancias
+#Llamados
+grafo = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+
+inicio = 'A'
+distancias = dijkstra(grafo, inicio)
+print("Distancias mínimas desde el nodo inicial:", distancias)
